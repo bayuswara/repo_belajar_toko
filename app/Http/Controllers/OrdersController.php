71c7069edc40f583922abb667f91c38d987f9ab6
biswara890@gmail.com <biswara890@gmail.com>
2020-09-18 08:38:28 +0700

    MENAMBAH FUNGSI DELETE

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -9,60 +9,74 @@ use Illuminate\Support\Facades\Validator;
 class OrdersController extends Controller
 {
   public function show(){
-    $data_orders = Orders::join('customers', 'customers.id_customer', 'orders.id_customer')->get();
+    $data_orders = Orders::join('customers', 'customers.id_customer', 'orders.id_order')->get();
+    $data_orders = Orders::join('product', 'product.id_product', 'orders.id_product')->get();
     return Response()->json($data_orders);
   }
   public function detail($id_order){
     if(Orders::where('id_order', $id_order)->exists()){
-    $data_orders = Orders::join('customers', 'customers.id_customer', 'orders.id_customer')->where('orders.id_order', '=', $id_order)->get();
-    return Response()->json($data_orders);
+      $data_orders = Orders::join('customers', 'customers.id_customer', 'orders.id_order')->where('order.id_order','=',$id_order)->get();
+      $data_orders = Orders::join('product', 'product.id_product', 'orders.id_product')->where('orders.id_order', '=', $id_order)->get();
+      return Response()->json($data_orders);
+    }
+    else{
+      return Response()->json(['message' => 'Tidak ditemukan']);
+    }
   }
-  else{
-    return Response()->json(['message' => 'Tidak ditemukan']);
-  }
-}
-  public function store(Request $request){
-    $validator=Validator::make($request->all(),
-    [
-      'nama_order' => 'required',
-      'tanggal_order' => 'required',
-      'alamat_order' => 'required',
-      'id_customer' => 'required'
-    ]
-  );
-  if($validator->fails()){
-    return Response()->json($validator->errors());
-  }
-  $simpan = Orders::create([
-    'nama_order' => $request->nama_order,
-    'tanggal_order' => $request->tanggal_order,
-    'alamat_order' => $request->alamat_order,
-    'id_customer' => $request->id_customer
-  ]);
-  if($simpan){
-    return Response()->json(['status'=>1]);
-  }
-  else{
-    return Response()->json(['status'=>0]);
-  }
-  }
-  public function update($id_order, Request $request){
+    public function store(Request $request){
+      $validator=Validator::make($request->all(),
+      [
+        'id_customer' => 'required',
+        'id_product' => 'required',
+        'tanggal_order' => 'required',
+        'berat' => 'required',
+        'total' => 'required'
+      ]);
+      if($validator->fails()){
+        return Response()->json($validator->errors());
+      }
+      $simpan = Orders::create([
+        'id_customer' => $request->id_customer,
+        'id_product' => $request->id_product,
+        'tanggal_order' => $request->tanggal_order,
+        'berat' => $request->berat,
+        'total' => $request->total
+      ]);
+      if($simpan){
+        return Response()->json(['status' => 1]);
+      }
+      else{
+        return Response()->json(['status' => 0]);
+      }
+    }
+    public function update($id_order, Request $request){
     $validator=Validator::make($request->all(),[
-      'nama_order' => 'required',
+      'id_customer' => 'required',
+      'id_product' => 'required',
       'tanggal_order' => 'required',
-      'alamat_order' => 'required',
-      'id_customer' => 'required'
+      'berat' => 'required',
+      'total' => 'required'
     ]);
     if($validator->fails()){
       return Response()->json($validator->errors());
     }
     $ubah = Orders::where('id_order', $id_order)->update([
-      'nama_order' => $request->nama_order,
+      'id_customer' => $request->id_customer,
+      'id_product' => $request->id_product,
       'tanggal_order' => $request->tanggal_order,
-      'alamat_order' => $request->alamat_order,
-      'id_customer' => $request->id_customer
+      'berat' => $request->berat,
+      'total' => $request->total
     ]);
     if($ubah){
+      return Response()->json(['status' => 1]);
+    }
+    else{
+      return Response()->json(['status' => 0]);
+    }
+  }
+  public function destroy($id_order){
+    $hapus = Orders::where('id_order', $id_order)->delete();
+    if($hapus){
       return Response()->json(['status' => 1]);
     }
     else{
--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/customers','CustomersController@show');
-Route::post('/customers','CustomersController@store');
 
-Route::get('/product','ProductController@show');
-Route::get('/product/{id_product}','ProductController@detail');
-Route::post('/product','ProductController@store');
-Route::put('/product/{id_product}','ProductController@update');
+Route::post('/customers', 'CustomersController@store');
+Route::get('/customers', 'CustomersController@show');
 
-Route::get('/orders','OrdersController@show');
-Route::get('/orders/{id_order}','OrdersController@detail');
-Route::post('/orders','OrdersController@store');
-Route::put('/orders/{id_order}','OrdersController@update');
+Route::post('/orders', 'OrdersController@store');
+Route::get('/orders', 'OrdersController@show');
+Route::get('/orders/{id}', 'OrdersController@detail');
+Route::put('/orders/{id}', 'OrdersController@update');
+Route::delete('/orders/{id}', 'OrdersController@destroy');
+
+Route::post('/product', 'ProductController@store');
+Route::get('/product', 'ProductController@show');
